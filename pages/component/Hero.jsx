466672f06e301091780449b420fa9c96943d0922
LUAN FREIRE ALVES DA SILVA <09270464156@ifms.edu.br>
2023-10-03 19:37:41 -0400

    chore: add hero section

---
+++ pages/component/Hero.jsx
@@ -0,0 +1,14 @@
+import style from '../../style/module/hero.module.css';
+import Link from 'next/link'
+
+export default function Hero() {
+    return (
+        <>
+            <div className={style.boxes}>
+                <Link className={style.box_single} href="/">
+                    Teste Hero!
+                </Link>
+            </div>
+        </>
+    );
+}
