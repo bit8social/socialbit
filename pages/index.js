69f7605120ab5bec77bc3b60e1f810f02b668529
Luan_ <88367567+Luan16p@users.noreply.github.com>
2023-10-05 14:15:31 -0400

    chore: change bg for static url

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
-import style from '../style/module/index.module.css'
-import Header from './component/Header'
+import style from '../style/module/index.module.css';
+import Header from './component/Header';
+import Linked from './component/Linked'
+
 import Head from 'next/head';
 
 export default function SocialBit() {
     return (
         <>
             <Head>
-                <title>SocialBit - Links</title>
+                <link rel="shortcut icon" href="https://github.com/Luan16p/socialbit/blob/main/assets/fav.png?raw=true" type="image/x-icon" />
+                <title>SocialBit</title>
             </Head>
+
             <main className={style.mainapp}>
-                <Header/>
+                <Header />
+                <Linked />
             </main>
         </>
-    );   
+    );
 }
