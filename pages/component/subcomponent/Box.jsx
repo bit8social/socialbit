cb77d9e563a624e75692fd5e31928806253487d6
Luan_ <88367567+Luan16p@users.noreply.github.com>
2023-10-21 10:45:12 -0400

    chore: update box.jsx

--- pages/component/subcomponent/Box.jsx
+++ pages/component/subcomponent/Box.jsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function Box({ titleProps = "Socialbit 'Links'", labelBox = "####Label Não definido", idLabelBox, IconBox, idIcon, idBox, URLRoute }) {
   return (
-    <Link className={style.box_single} id={idBox} href={URLRoute || "www.github.com"} title={titleProps}>
-      <IconBox className={style.iconbox} id={idIcon} />
-      <div className={style.labels}>
-        <span className={style.label_}>{labelBox}</span>
-        <span className={style.labelbox}>{idLabelBox || null}</span>
-      </div>
-    </Link>
+    <>
+      <Link className={style.box_single} id={idBox} href={URLRoute || "www.github.com"} title={titleProps}>
+        <IconBox className={style.iconbox} id={idIcon} />
+        <div className={style.labels}>
+          <span className={style.label_}>{labelBox}</span>
+          <span className={style.labelbox}>{idLabelBox || null}</span>
+        </div>
+      </Link>
+    </>
   );
 }
